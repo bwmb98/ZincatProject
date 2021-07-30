7cf07a8c7cc106229097ce17ee3a906b6cd35c38
bwmb98 <manulbinusha890@gmail.com>
2021-07-30 08:49:33 +0530

    newback

--- src/main/java/com/example/app/controller/Controller.java
+++ src/main/java/com/example/app/controller/Controller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.app.model.User;
+import com.example.app.response.ResponseEP;
 import com.example.app.service.RegServices;
 
 
@@ -19,75 +20,16 @@ public class Controller {
 	private RegServices service;
 	
 	@PostMapping("/userRegister")
-//	@CrossOrigin(origins = "http://localhost:4200")
 
-	public User registerUser(@RequestBody User user) throws Exception{
-
-		String checkEmail = user.getEmail();
-			if (checkEmail != null && !"".equals(checkEmail)) {
-				User userobj= service.checkuseremail(checkEmail);
-					if (userobj != null) {
-						throw new Exception ("User is already exist");
-					}
-			}
+	public ResponseEP registerUser(@RequestBody User user){
 		
-
-		User userObj=null;
-		userObj=service.saveUser(user);
-		
-		return userObj;
+		return service.saveUser(user);
 	}
 	
-	
-//	@PostMapping("/userReg")
-//	public ResponseEP newUser(@RequestBody User user) {
-//		ResponseEP res = null;
-//		try {
-//			// service + business logic
-//			String checkEmail = user.getEmail();
-//			if (checkEmail != null && !"".equals(checkEmail)) {
-//				User userobj= service.checkuseremail(checkEmail);
-//					if (userobj != null) {
-//						res = new ResponseEP();
-//						res.setMessageString("User already exist");
-//						res.setStatusCode(500);
-//						//res.setUserid(userobj.getUserid());
-//					}else {
-//						service.saveUser(user);
-//						res = new ResponseEP();
-//						res.setMessageString("User saved successfully");
-//						res.setStatusCode(200);
-//						//res.setUserid(1);
-//					}
-//			}
-//		}catch(Exception ex) {
-//			// what is your error response
-//			res = new ResponseEP();
-//			res.setMessageString("Error in user saving");
-//			res.setStatusCode(405);
-//			//res.setUserid(0);
-//			
-//		}
-//		return res;
-//	}
-	
-	
 	@PostMapping("/userLogin")
-//	@CrossOrigin(origins = "http://localhost:4200")
 
-	
-	public User loginUser(@RequestBody User user) throws Exception {
+	public ResponseEP loginUser(@RequestBody User user) throws Exception {
 		
-		String tempEmail = user.getEmail();
-		String tempPassword = user.getPassword();
-			User userobj = null;
-				if (tempEmail != null && tempPassword != null) {
-					userobj = service.checkuseremailnPassword(tempEmail, tempPassword);
-				}
-						if (userobj == null) {
-							throw new Exception ("Invalid User");
-						}
-						
-			return userobj;
+		return service.loginUser(user);
 		}
 	}
--- src/main/java/com/example/app/service/RegServices.java
+++ src/main/java/com/example/app/service/RegServices.java
@@ -5,25 +5,84 @@ import org.springframework.stereotype.Service;
 
 import com.example.app.model.User;
 import com.example.app.repository.RegRepository;
+import com.example.app.response.ResponseEP;
 
 @Service
 public class RegServices {
-	
+
 	@Autowired
 	private RegRepository repos;
-	
-	public User saveUser(User user) {
-		
-		return repos.save(user);
+
+	public ResponseEP saveUser(User user) {
+		ResponseEP rep = null;
+		try {
+			// service + business logic
+			String checkEmail = user.getEmail();
+			if (checkEmail != null && !"".equals(checkEmail)) {
+				User userobj = checkuseremail(checkEmail);
+				if (userobj != null) {
+					rep = new ResponseEP();
+					rep.setMessageString("User already exist");
+					rep.setStatusCode(500);
+					rep.setUserid(userobj.getUserid());
+				} 
+				else {
+					repos.save(user);
+					rep = new ResponseEP();
+					rep.setMessageString("User saved successfully");
+					rep.setStatusCode(200);
+					rep.setUserid(1);
+				}
+			}
+		} catch (Exception ex) {
+			// what is your error response
+			rep = new ResponseEP();
+			rep.setMessageString("Error in user saving");
+			rep.setStatusCode(405);
+			rep.setUserid(0);
+
+		}
+		return rep;
 	}
-	
+
 	public User checkuseremail(String email) {
 		return repos.findByEmail(email);
+	}
+
+	public ResponseEP loginUser(User user) {
+		ResponseEP rep = null;
+		try {
+			// service + business logic
+			String tempEmail = user.getEmail();
+			String tempPassword = user.getPassword();
+			User userobj = null;
+			if (tempEmail != null && tempPassword != null) {
+				userobj= checkuseremailnPassword(tempEmail,tempPassword);
+			}
+				if (userobj == null) {
+					rep = new ResponseEP();
+					rep.setMessageString("Invalid user");
+					rep.setStatusCode(401);
+					}
+				else {
+					rep = new ResponseEP();
+					rep.setMessageString("User logged-in successfully");
+					rep.setStatusCode(200);
+					rep.setUserid(1);
+				}
+		} catch (Exception ex) {
+			// what is your error response
+			rep = new ResponseEP();
+			rep.setMessageString("Error in login");
+			rep.setStatusCode(299);
+			rep.setUserid(0);
+
+		}
+		return rep;
 	}
 	
 	public User checkuseremailnPassword(String email, String password) {
 		return repos.findByEmailAndPassword(email, password);
 	}
-	
+
 }
-		
